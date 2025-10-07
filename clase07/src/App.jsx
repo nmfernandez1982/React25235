5225e2556dfe1e472a37ec817193a623cb685e15
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2025-10-06 23:39:57 -0300

    Clase07

---
+++ clase07/src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './paginas/Login';
+import Perfil from './paginas/Perfil';
+import Productos from './paginas/Productos';
+import Administracion from './paginas/Administracion';
+import NoEncontrado from './paginas/NoEncontrado';
+import RutaProtegida from './components/RutaProtegida';
+import Inicio from './paginas/inicio';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/perfil/:id" element={
+          <RutaProtegida><Perfil /></RutaProtegida>
+        } />
+        <Route path="/admin" element={
+          <RutaProtegida><Administracion /></RutaProtegida>
+        } />
+        <Route path="*" element={<NoEncontrado />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
