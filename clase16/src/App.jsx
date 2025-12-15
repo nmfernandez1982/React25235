553a375313e86ac407cf48cb2e2d4f56f30a6ed6
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2025-12-15 20:53:11 -0300

    Clase16

---
+++ clase16/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import FlipCards from './components/FlipCards';
+import FlipCatalog from './components/FlipCatalog';
+
+function App() {
+
+  return (
+    // <FlipCards /> 
+    <FlipCatalog /> 
+  )
+}
+
+export default App
